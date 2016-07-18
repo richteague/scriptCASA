7757f65bbeb1741b0509e0f2c8aa0b2cf88853ca
Rich Teague <teague@mpia.de>
2016-07-18 16:04:25 +0200

    initial commit

--- casa_simobs.py
+++ casa_simobs.py
@@ -3,15 +3,6 @@ import sys
 import numpy as np
 import scipy.constants as sc
 
-'''
-
-    Run in CASA with:
-    >>> execfile('casa_simobs.py')
-
-'''
-
-
-
 def runSimobs(beamsizes, inttimes, files=None, mininttime=100.):
 
     '''
@@ -54,8 +45,8 @@ def runSimobs(beamsizes, inttimes, files=None, mininttime=100.):
         header = imhead(imname, mode='list')
         ia.open(imname)
 
-        # For each permutation of integration time and beamsize, run simobserve()
-        # and then simanalyze() to get workable images.
+        # For each permutation of integration time and beamsize, run 
+        # simobserve() and then clean() to get workable images.
 
         for itime in inttimes:
             for bs in beamsizes:
@@ -74,11 +65,10 @@ def runSimobs(beamsizes, inttimes, files=None, mininttime=100.):
                 if header['bunit'].lower() == 'jy/pixel':
                     jy2k = 1.
                 else:
-                    jy2k = 10**-26 * sc.c**2. / 2. / header['restfreq']**2.
-                    jy2k /= sc.k * np.radians(header['cdelt2'])**2.
+                    jy2k = 2.266 * 10**26 * sc.k * header['cdelt2']**2.
+                    jy2k *= header['restfreq']**2. / sc.c**2.
 
-               inbright = '%5.3eJy/pixel' % (header['datamax']/jy2k)
-               print 'Rescaling to peak flux of %s Jy/pix.' % inbright
+                inbright = '%5.3eJy/pixel' % (header['datamax'] * jy2k)
 
                 # The integration time is the number of 'data points' used for the 
                 # simulated observation, hence CASA will essentially simulate
@@ -87,9 +77,9 @@ def runSimobs(beamsizes, inttimes, files=None, mininttime=100.):
                 # very slow.
 
                 incell      = '%3farcsec' % (header['cdelt2'] * 206265.)
-                integration = '%fs' % min(mininttime, inttime * 60)            
-                totaltime   = '%f.0s' % (inttime * 60)
-                antennalist = 'alma;%farcsec' % beamsize
+                integration = '%fs' % min(mininttime, itime * 60)            
+                totaltime   = '%f.0s' % (itime * 60)
+                antennalist = 'alma;%farcsec' % bs
                 thermalnoise= 'tsys-atm'
                 graphics    = 'file'
 
@@ -97,40 +87,38 @@ def runSimobs(beamsizes, inttimes, files=None, mininttime=100.):
 
                 simobserve()
 
-                # Set up the cleaning. TODO: More options here.
+                # Set up the cleaning.
+                # Find the antenna list with the closest angular size.
 
                 os.chdir(r'%s/' % project)
                 default('clean')
-                vis         = project +'.'+antennalist.replace(';','_') +'.noisy.ms'
-                imagename   = 'CLEANed'
+                vis = project +'.'+antennalist.replace(';','_') +'.noisy.ms'
+                imagename = 'CLEANed'
 
                 # By default, clean all native channels.
 
-                mode        = 'velocity'
-                nchan       = -1
-                start       = ''
-                width       = ''
+                mode = 'velocity'
+                nchan = -1
+                start = ''
+                width = ''
 
-                # Set the level of appropriate cleaning.
-                
-                niter       = 10000
-                threshold   = '0.1mJy'          
-                restfreq    = '%fGHz'           % (header['restfreq'] / 1e9)
+                # Set the level of appropriate cleaning and run.
+                # More options are needed here.
+
+                niter = 10000
+                threshold = '0.1mJy'          
+                restfreq = '%fGHz' % (header['restfreq'] / 1e9)
                 interactive = False
-                imsize      = 256               # 
-                cell        = '%.5farcsec'      % (header['cdelt2'] * 206265. * 2.5)
+                imsize = 256                
+                cell = '%.5farcsec' % (header['cdelt2'] * 206265.)
                 phasecenter = 0
-                weighting   = 'briggs'
-                robust      = 0.5
-                pbcor       = False
-                outframe    = 'LSRK'
-                
-                # Run clean(). 
-
+                weighting = 'briggs'
+                robust = 0.5
+                pbcor = False
+                outframe = 'LSRK'
                 clean()
 
-                # Clean up the files.
-
+                # Clean up the files and move back into working directory.
                 os.system('rm *.txt')
                 os.system('rm *.png')
                 os.system('rm *.last')
@@ -138,60 +126,22 @@ def runSimobs(beamsizes, inttimes, files=None, mininttime=100.):
 
 
                 # Export as a .fits file.
-
-                exportfits(imagename = project + r'/' + imagename + '.image',
+                exportfits(imagename = imagename + '.image',
                            fitsimage = (filein[:-5] + '_' + str(beamsize) + 
                                         'arcsec_' + str(inttime) + 'mins_simobs.fits'),
                            overwrite = True,
-                           dropstokes= True,
-                           velocity  = True)
+                           dropstokes = True,
+                           velocity = True)
+
+                # Clean up files.
+                os.system(r'rm *.last')
+                os.system(r'rm -rf %s' % imname)
+                os.system(r'rm -rf %s' % project)
+                os.system(r'rm *.log')
 
     return
 
 
-
-
-for f, filein in enumerate(files):
-
-    #--IMPORT FITS -----------------------------------------------------------------
-
-    # Load the fits file.
-    # Note that velocity axes are automatically changed to frequency.
-    # The angular sizes are converted to radians but needs to be input as arcsec.
-
-
-    # Loop through each permutation of the build.
-    for inttime in inttimes:
-        for beamsize in beamsizes:
-
-            #-- SIMOBSERVE ---------------------------------------------------------
-
-
-
-
-            #-- SIMANALYZE ---------------------------------------------------------
-
-            # Set up the CLEAN parameters. Change as necessary.
-
-
-            
-            
-    #-------------------------------------------------------------------------------
-
-    # Clean up files.
-    os.system(r'rm *.last')
-    #os.system(r'mv *.fits ../')
-    #os.chdir(r'../')
-    #os.system(r'rm *.log')
-
-    
-    if not os.path.isdir(filein[:-5]+'_Outputs'): os.mkdir(filein[:-5]+'_Outputs')
-    os.system(r'mv %s*.fits ./%s_Outputs/' % (filein[:-5], filein[:-5]))
-
-    if not os.path.isdir('SimObs_'+filein[:-5]): os.mkdir('SimObs_'+filein[:-5])
-    os.system(r'mv %s* ./SimObs_%s/' % (filein[:-5], filein[:-5]))
-
-    print 'Completed: %d / %d.' % (f+1, len(files))
 
 
 
